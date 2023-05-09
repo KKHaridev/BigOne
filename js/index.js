099760a4e8e3383907e1bab5a3c5a1a8cfbba671
K K Haridev <kkharidev@gmail.com>
2023-05-09 20:22:23 +0530

    Updated some functionalities

--- js/index.js
+++ js/index.js
@@ -19,6 +19,7 @@ navLinks.forEach((item) => {
 
 const h1 = document.querySelectorAll("h1");
 const heading = document.querySelectorAll("h2");
+const h3 = document.querySelectorAll(".h3");
 const subheading = document.querySelectorAll("h4");
 const paragraph = document.querySelectorAll(".why-content");
 const clients = document.querySelectorAll(".clients");
@@ -41,6 +42,7 @@ const observer = new IntersectionObserver(
 );
 btn.forEach((el) => observer.observe(el));
 h1.forEach((el) => observer.observe(el));
+h3.forEach((el) => observer.observe(el));
 
 clients.forEach((el) => observer.observe(el));
 paragraph.forEach((el) => observer.observe(el));
@@ -49,6 +51,25 @@ heading.forEach((el) => observer.observe(el));
 cards.forEach((el) => observer.observe(el));
 
 const copyright = document.querySelector(".copyright");
-console.log(copyright);
 const year = new Date().getFullYear();
-copyright.innerHTML = ` &copy; ${year} All Rights Reserved`;
+if(copyright){
+  copyright.innerHTML = ` &copy; ${year} All Rights Reserved`;
+}
+
+
+
+
+function sendMsg(e) {
+  e.preventDefault();
+  const modal=document.querySelector(".modal")
+  setTimeout(()=>{
+    modal.classList.remove("hiddenModal")
+  },1000);
+  
+
+}
+
+const loaderContainer = document.querySelector('.loader');
+window.addEventListener('load', () => {
+    loaderContainer.parentElement.removeChild(loaderContainer);
+});
